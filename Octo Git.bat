cb6695f611d5cc57fe21dcdce42b3e4d1c578b4e
Aavin sidhu <aavinsidhu@hotmail.com>
2018-01-21 16:15:13 -0500

    help

--- Octo Git.bat
+++ Octo Git.bat
@@ -1,33 +1,61 @@
 @echo off
+
 echo type "commit" "pull" "exit"
+
 cd curl
 
-set GIT_PATH "C:\Program Files\Git\git-cmd.exe"
+
+
 set BRANCH = "origin"
+
+
 
 :P
 
+
+
 set ACTION=
+
+
 
 set /P ACTION=Action: %=%
 
+
+
 if "%ACTION%"=="commit" (
+
 	
+
 	call git add *
+
 	call git commit -m "commited with Octo Git on %date%"
-	call git push %BRANCH%
+
+	call git push
+
+
 
 )
+
+
 
 if "%ACTION%"=="pull" (
 
-	call git pull %BRANCH%
+
+
+	call git pull
+
+
 
 )
 
+
+
 )
+
+
 
 if "%ACTION%"=="exit" exit /b
 
+
+
 goto P
-Raw 
